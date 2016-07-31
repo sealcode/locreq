60686e79fb4554eeb453be099cd7fd198aae4848
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-07-31 14:29:20 +0200

    Curry the main function for ease of use

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function fileExists(filePath) {
     } catch (err) {
         return false;
     }
-}
+};
 
 function search_for_file_upwards(start, filename){
 	let current_path = path.resolve(start, "./" + filename)
@@ -15,12 +15,18 @@ function search_for_file_upwards(start, filename){
 		current_path = path.resolve(current_path, "../../" + filename);
 	}
 	return current_path;
-}
+};
 
 function locreq(caller_path, module_path){
 	const package_path = search_for_file_upwards(caller_path, "package.json");
 	const to_require = path.resolve(package_path, "../", module_path);
 	return require(to_require);
-}
+};
 
-module.exports = locreq;
+function curry_locreq(caller_path){
+	return function(module_path){
+		return locreq(caller_path, module_path);
+	};
+};
+
+module.exports = curry_locreq;
